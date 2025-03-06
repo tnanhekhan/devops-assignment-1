bd41c06560dc7ee09958379979e1569da266540e
Tabish <32935492+tnanhekhan@users.noreply.github.com>
2025-03-06 03:32:04 +0100

    added rest of crud

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -3,7 +3,7 @@ import six
 
 from swagger_server.models.student import Student  # noqa: E501
 from swagger_server import util
-
+from swagger_server.service.student_service import *
 
 def add_student(body=None):  # noqa: E501
     """Add a new student
@@ -13,11 +13,12 @@ def add_student(body=None):  # noqa: E501
     :param body: Student item to add
     :type body: dict | bytes
 
-    :rtype: str
+    :rtype: float
     """
     if connexion.request.is_json:
         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        return add(body)
+    return 500,'error'
 
 
 def delete_student(student_id):  # noqa: E501
@@ -30,7 +31,7 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: object
     """
-    return 'do some magic!'
+    return delete(student_id)
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -43,4 +44,4 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    return get_by_id(student_id)
