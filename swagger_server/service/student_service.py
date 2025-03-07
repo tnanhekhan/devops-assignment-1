ba56a344585b272a1cec2911cadc7b1d5b5491e2
Tabish <32935492+tnanhekhan@users.noreply.github.com>
2025-03-07 04:14:55 +0100

    trigger build

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -11,18 +11,18 @@ def add(student=None):
     student_dict = student.to_dict()
     result = student_collection.insert_one(student_dict)
     student.student_id = str(result.inserted_id)
-    return student.student_id, 200
+    return student.student_id
 
 def get_by_id(student_id=None, subject=None):
     student = student_collection.find_one({"_id": student_id})
     if not student:
         return 'not found', 404
     student['student_id'] = str(student['_id'])
-    return student, 200
+    return student
 
 def delete(student_id=None):
     student = student_collection.find_one({"_id": student_id})
     if not student:
         return 'not found', 404
     student_collection.delete_one({"_id": student_id})
-    return student_id, 200
+    return student_id
